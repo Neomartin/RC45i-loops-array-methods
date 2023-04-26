8d6c3b0f0d78ff70ed65bcc9faefd534d226e42e
Martin Rodriguez <neomartinr@gmail.com>
2023-04-26 19:56:52 -0300

    For loops finalizado

---
+++ js/arrays/for-of.js
@@ -0,0 +1,38 @@
+const paisesLatam = [
+    "Argentina",
+    "Bolivia",
+    "Brasil",
+    "Chile",
+    "Colombia",
+    "Ecuador",
+    "Guyana",
+    "Guyana Francesa",
+    "Paraguay",
+    "Perú",
+    "Suriname",
+    "Uruguay",
+    "Venezuela",
+    "Guyana",
+    "Guyana Francesa",
+    "Paraguay",
+    "Perú",
+    "Suriname",
+    "Uruguay",
+    "Venezuela",
+];
+
+for(let pais of paisesLatam) {
+
+    console.log(pais)
+    
+}
+
+
+// for(let i = 0; i < paisesLatam.length; i++ ) {
+//     let numero = (paisesLatam.length - i); //resultado numérico
+//     const numeroToString = String(numero);
+//     const numeroTwoDigits = numeroToString.padStart(2, '0');
+
+//     document.write(`<p>${numeroTwoDigits} - ${paisesLatam[i]}</p>`);
+
+// }
